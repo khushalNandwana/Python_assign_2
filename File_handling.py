696ead8884ee726a950b2671b563c09acd5d73af
Khushal Nandwana <161406407+khushalNandwana@users.noreply.github.com>
2025-03-12 19:20:50 +0530

    Add files via upload

---
+++ File_handling.py
@@ -0,0 +1,24 @@
+import pickle
+record=[]
+def write():
+    file=open(r'C:\Users\khush\Documents\kevon projects\Python_assign_2\student.txt','w')
+    while True:
+        name=input("Enter the name:")
+        roll_no= int(input("Enter the roll No:"))
+        marks=float(input("Enter the marks:"))
+        data=[name,str(roll_no),str(marks)]
+        record.append(data)#Nested List
+        ch=input("Y->Yes\nN->No\nEnter your choice")
+        if ch=='n' or ch=='N':
+            break
+    pickle.dump(record,file)
+def read():
+    file=open(r'C:\Users\khush\Documents\kevon projects\Python_assign_2\student.txt','r')
+    s=pickle.load(file)
+    for i in s:
+        print(i)
+#main function
+write()
+print("Data written Successfully...")
+read()
+print("Program over..")
