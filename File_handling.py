424dc5fd30a171b60a4ea611598282d0c56fccda
Khushal Nandwana <161406407+khushalNandwana@users.noreply.github.com>
2025-03-12 21:09:30 +0530

    Update File_handling.py

--- File_handling.py
+++ File_handling.py
@@ -1,30 +1,24 @@
 def write():
     file= open(r"C:\Users\khush\Documents\kevon projects\Python_assign_2\file_handling\students.txt", "w")
     while True:
+        try:
             name = input("Enter student name: ")
-            if name.lower() == 'done':
-                break
-            try:
-                age = int(input("Enter student age: "))
-                marks = float(input("Enter student marks: "))
-            except ValueError:
+            age = int(input("Enter student age: "))
+            marks = float(input("Enter student marks: "))
+        except ValueError:
                 print("Error = Please insert correct input")
                 continue
-            ch = input("Y->Yes\nN->No\nEnter your choice")
-            if ch == 'n' or ch == 'N':
-                break
-    file.write(f"Name: {name}, Age: {age}, Marks: {marks}\n")
+        ch = input("Y->Yes\nN->No\nEnter your choice")
+        if ch == 'n' or ch == 'N':
+            break
+    file.write(f"Name={name}, Age={age}, Marks={marks}\n")
 
 
 def read():
-    try:
-        file=open(r"C:\Users\khush\Documents\kevon projects\Python_assign_2\file_handling\students.txt", "r")
-        contents = file.read()
-        print("\nStudent Information")
-        print(contents)
-    except FileNotFoundError:
-        print("Error: students.txt not found.")
-
+    file=open(r"C:\Users\khush\Documents\kevon projects\Python_assign_2\file_handling\students.txt", "r")
+    contents = file.read()
+    print("\nStudent Information")
+    print(contents)
 
 # Main program execution
 write()
