b382ea027b7154736b0711fdce6487bff1d5ec6e
Khushal Nandwana <161406407+khushalNandwana@users.noreply.github.com>
2025-03-19 10:21:46 +0530

    Update chuckNorris_api.py

--- chuckNorris_api.py
+++ chuckNorris_api.py
@@ -1,11 +1,11 @@
 import requests
-def chuck_norris_joke():
-    url = "https://api.chucknorris.io/jokes/random"
 
-    response = requests.get(url)
+url = "https://api.chucknorris.io/jokes/random"
 
-    if response.status_code == 200:
-        joke = response.json()["value"]
-        print("Chuck Norris Joke:", joke)
-    else:
-        print("Failed")
+response = requests.get(url)
+
+if response.status_code == 200:
+    joke = response.json()
+    print("Chuck Norris Joke:", joke["value"])
+else:
+    print("Failed")
